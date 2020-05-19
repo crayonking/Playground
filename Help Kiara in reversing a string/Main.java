6441082d87bccd1841f7fff5f1f221aee7638ff3
crayonking <64580326+crayonking@users.noreply.github.com>
2020-05-19 21:14:00 +0530

    Test code pushed to repository.

---
+++ Help Kiara in reversing a string/Main.java
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int main() 
+{ 
+   string str;
+  getline(cin,str);
+  
+    // Reverse str[beign..end] 
+    reverse(str.begin(), str.end()); 
+  
+    cout << str; 
+    return 0; 
+} 
---
+++ Hop n Hop/Main.java
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+int x,y,a=3,b=4;
+  cin>>x>>y;
+ int c=(x-a);
+ int d=(y-b);
+ int e=c*c;
+ int f=d*d;
+  int z=e+f;
+ int s=sqrt(z);
+  cout<<s;
+}
